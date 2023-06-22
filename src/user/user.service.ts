b6ab5eef08feac7505603f5a9b16bec169917f0b
박종유 <jypark@nextree.io>
2023-06-22 18:00:21 +0900

    feat: Prisma Client 사용하기

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../generated/nestjs-dto/user/dto/create-user.dto';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class UserService {
   // constructor(private userRepository: UserRepository) {}
 
   async insertUser(userDto: CreateUserDto) {
-    console.log('userDto', userDto);
+    prisma.user.create({
+      data: { ...userDto },
+    });
   }
 }
